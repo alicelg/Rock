3ced093c5f368ed7f0ebc80993451f38d498c3ce
alicelg <alicelop@ucm.es>
2021-02-11 17:13:10 +0100

    función visualizar detail

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Artists, ArtistsService } from '../services/artists.service';
+import { Artist, ArtistService } from '../services/artists.service';
 
 
 @Component({
@@ -11,21 +11,21 @@ import { Artists, ArtistsService } from '../services/artists.service';
 export class DetailComponent implements OnInit {
 
 
-  artistsSelect: Artists
+  selectedArtist: Artist
 
 
   constructor(
-    private artistsService: ArtistsService,
     private activateRoute: ActivatedRoute,
+    private artistsService: ArtistService,
     private router: Router) { }
 
   ngOnInit(): void {
 
-    const artistsId = Number(this.activateRoute.snapshot.paramMap.get('artistsId'))
-    console.log(artistsId);
-    this.artistsService.getById(artistsId).then(artist => {
+    const artistId = Number(this.activateRoute.snapshot.paramMap.get('artistId'))
+    console.log(artistId);
+    this.artistsService.getById(artistId).then(artist => {
       if (artist) {
-        this.artistsSelect = artist
+        this.selectedArtist = artist
       } else {
         this.router.navigate(['error'])
       }
