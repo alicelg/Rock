2c60e3d8178a47a83c20550fd68bbd9b4ab62ce1
alicelg <alicelop@ucm.es>
2021-02-11 11:59:11 +0100

    lista de artistas inicial

---
+++ src/assets/data/artists.ts
@@ -0,0 +1,107 @@
+export const Artists = [
+    {
+        id: 0,
+        band: "The Rolling Stones",
+        origin: "Reino Unido",
+        description: "Es una banda británica de rock originaria de Londres. La banda se formó en abril de 1962 por Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Charlie Watts e Ian Stewart. Brian Jones fue despedido en junio de 1969, falleciendo tres semanas después, siendo reemplazado por el guitarrista Mick Taylor, que dejaría el grupo en 1975 y sería a su vez reemplazado por Ronnie Wood. Con el retiro de Bill Wyman en 1993 se incluyó al bajista Darryl Jones que, aunque toca con la banda desde la grabación del álbum Voodoo Lounge en 1994, no es un miembro oficial.",
+        image: "https://www.ecured.cu/images/thumb/2/25/Kiss.jpg/260px-Kiss.jpg",
+        spotify: "https://open.spotify.com/playlist/2M7YeNwuRkDfTJYYwhPCkR",
+        imusic: "https://music.apple.com/es/artist/the-rolling-stones/1249595",
+        youtube: "https://www.youtube.com/watch?v=YzjZEci-EiU&ab_channel=ClassicRockCollection ",
+        web: "https://rollingstones.com/",
+        members: "Mick Jagger, Keith Richards, Charlie Watts, Ronnie Wood",
+        exmembers: "Brian Jones, Ian Stewart, Bill Wyman, Mick Taylor, Dick Taylor, Tony Chapman, Mick Avory"
+    },
+
+    {
+        id: 1,
+        band: "Led Zeppelin",
+        origin: "",
+        description: "",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/49/LedZeppelinmontage.jpg",
+        spotify: "",
+        imusic: "",
+        youtube: "",
+        members: "",
+        exmembers: ""
+    },
+
+    {
+        id: 2,
+        band: "Queen",
+        origin: "",
+        description: "",
+        image: "",
+        spotify: "",
+        imusic: "",
+        youtube: "",
+        members: "",
+        exmembers: ""
+    },
+
+    {
+        id: 3,
+        band: "Pink Floyd",
+        origin: "",
+        description: "",
+        image: "",
+        spotify: "",
+        imusic: "",
+        youtube: "",
+        members: "",
+        exmembers: ""
+    },
+
+    {
+        id: 4,
+        band: "Deep Purple",
+        origin: "",
+        description: "",
+        image: "",
+        spotify: "",
+        imusic: "",
+        youtube: "",
+        members: "",
+        exmembers: ""
+    },
+
+    {
+        id: 5,
+        band: "AC/DC",
+        origin: "",
+        description: "",
+        image: "",
+        spotify: "",
+        imusic: "",
+        youtube: "",
+        members: "",
+        exmembers: ""
+    },
+
+    {
+        id: 6,
+        band: "The Ramones",
+        origin: "",
+        description: "",
+        image: "",
+        spotify: "",
+        imusic: "",
+        youtube: "",
+        members: "",
+        exmembers: ""
+    },
+
+    {
+        id: 7,
+        band: "Kiss",
+        origin: "",
+        description: "",
+        image: "",
+        spotify: "",
+        imusic: "",
+        youtube: "",
+        members: "",
+        exmembers: ""
+    }
+
+]
