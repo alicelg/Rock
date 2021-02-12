68e02739569a3faae8fe87e133b90c7b1c508bbd
alicelg <alicelop@ucm.es>
2021-02-12 16:47:42 +0100

    Añado traducciones

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public translateService: TranslateService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  changeLanguage(lang: string): void {
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
+    localStorage.setItem('language', lang);
+  }
+
 }
+
+
+
+
