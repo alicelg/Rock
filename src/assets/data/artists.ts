b02bf1aecef51d2df8182465e4e21a51a0df01cf
alicelg <alicelop@ucm.es>
2021-02-13 16:59:12 +0100

    cambios de enlaces de youtube

--- src/assets/data/artists.ts
+++ src/assets/data/artists.ts
@@ -19,9 +19,9 @@ export const initArtists = [
         origin: "Reino Unido",
         description: "GENERAL.TWO-TEXT-DESCRIPTION",
         image: "../../assets/image/led.jpg",
-        spotify: "https://open.spotify.com/playlist/2MTWsZURWTG0kPXVLPMxGH ",
+        spotify: "https://open.spotify.com/playlist/2MTWsZURWTG0kPXVLPMxGH",
         imusic: "https://music.apple.com/es/artist/led-zeppelin/994656",
-        youtube: "https://www.youtube.com/watch?v=sfR_HWMzgyc&list=RDEMo1pnOuN0J4ZzmIfrwxpP0w&start_radio=1&ab_channel=OzWho",
+        youtube: "https://www.youtube.com/embed/sfR_HWMzgyc",
         members: "Jimmy Page, Robert Plant, John Paul Jones, John Bonham",
         exmembers: null,
         web: "https://lz50.ledzeppelin.com/?ref=https://www.google.com/"
@@ -35,7 +35,7 @@ export const initArtists = [
         image: "../../assets/image/queen.png",
         spotify: "https://open.spotify.com/playlist/595cY5S1FpMpiEIWsk6dG7",
         imusic: "https://music.apple.com/es/artist/queen/3296287",
-        youtube: "https://www.youtube.com/watch?v=rY0WxgSXdEE&list=RDEMbHaAxpOZhcVmmF6I3y0siA&start_radio=1&ab_channel=QueenOfficial",
+        youtube: "https://www.youtube.com/embed/rY0WxgSXdEE",
         members: "Brian May, Roger Taylor",
         exmembers: "Freddie Mercury, John Deacon",
         web: "https://www.queenonlinestore.com/ "
@@ -49,7 +49,7 @@ export const initArtists = [
         image: "../../assets/image/pink.jpg",
         spotify: "https://open.spotify.com/playlist/37i9dQZF1DXaQ34lqGBfrU ",
         imusic: "https://music.apple.com/es/artist/pink-floyd/487143",
-        youtube: "https://www.youtube.com/watch?v=2YopyMU2SYA&ab_channel=PaulA.Parris",
+        youtube: "https://www.youtube.com/embed/2YopyMU2SYA",
         members: "David Gilmour, Nick Mason, Richard Wright",
         exmembers: "Syd Barrett, Roger Waters, Bob Klose",
         web: "https://www.pinkfloyd.com/home.php"
@@ -63,7 +63,7 @@ export const initArtists = [
         image: "../../assets/image/deep.jpg",
         spotify: "https://open.spotify.com/playlist/37i9dQZF1DX5WtNRbfbuUo ",
         imusic: "https://music.apple.com/es/artist/deep-purple/135532 ",
-        youtube: "https://www.youtube.com/watch?v=mhqeZM-X9-k&ab_channel=MusicOldies",
+        youtube: "https://www.youtube.com/embed/mhqeZM-X9-k",
         members: "Ian Paice, Ian Gillan, Roger Glover, Steve Morse, Don Airey",
         exmembers: "Jon Lord, Ritchie Blackmore, Rod Evans, Nick Simper, Glenn Hughes, David Coverdale, Tommy Bolin, Joe Lynn Turner, Joe Satriani",
         web: "https://deeppurple.com/"
@@ -77,7 +77,7 @@ export const initArtists = [
         image: "../../assets/image/ac.jpg",
         spotify: "https://open.spotify.com/playlist/37i9dQZF1DXec50AjHrNTq ",
         imusic: "https://music.apple.com/es/artist/ac-dc/5040714",
-        youtube: " https://www.youtube.com/watch?v=v2AC41dglnM&list=RDEMDs8vWIQKMflBG8QUQQaUrw&start_radio=1&ab_channel=acdcVEVO",
+        youtube: "https://www.youtube.com/embed/v2AC41dglnM",
         members: "Angus Young, Stevie Young, Brian Johnson, Cliff Williams, Phil Rudd",
         exmembers: "Bon Scott, Malcolm Young, Simon Wright,Mark Evans,Dave Evans",
         web: " http://www.ac-dc.net/"
@@ -91,7 +91,7 @@ export const initArtists = [
         image: "../../assets/image/ramones.jpg",
         spotify: "https://open.spotify.com/artist/1co4F2pPNH8JjTutZkmgSm",
         imusic: "https://music.apple.com/es/artist/ramones/60715",
-        youtube: "https://www.youtube.com/watch?v=F3J0iwwsq-w&list=PLMEZyDHJojxMRL0ifs67xcfzTOiPrGMdF&ab_channel=BoPTePegar",
+        youtube: "https://www.youtube.com/embed/F3J0iwwsq-w?start=21",
         members: "Joey Ramone, Johnny Ramone,Marky Ramone, Elvis Ramone,C. J. Ramone,Dee Dee Ramone, Ramone, Richie Ramone",
         exmembers: null,
         web: "https://www.ramones.com/"
@@ -105,7 +105,7 @@ export const initArtists = [
         image: "https://www.ecured.cu/images/thumb/2/25/Kiss.jpg/260px-Kiss.jpg",
         spotify: "https://open.spotify.com/artist/07XSN3sPlIlB2L2XNcTwJw",
         imusic: "https://music.apple.com/es/artist/kiss/118108",
-        youtube: "https://www.youtube.com/watch?v=lat7m_LIAl8&list=PLwDmVi51SV_Hy6Kl5TrfM3fTo-_Z1a634&ab_channel=emblem96",
+        youtube: "https://www.youtube.com/embed/sDCw4fj_TsA",
         members: "Paul Stanley, Gene Simmons, Eric Singer, Tommy Thayer",
         exmembers: "Ace Frehley, Peter Criss, Eric Carr, Vinnie Vincent, Mark St. John, Bruce Kulick",
         web: "https://www.kissonline.com/"
