0dcfe7927014281b42c02c2a96c37b7bcc155d55
alicelg <alicelop@ucm.es>
2021-02-13 17:26:42 +0100

    Auto-generated commit

---
+++ assets/data/artists.ts
@@ -0,0 +1,114 @@
+export const initArtists = [
+    {
+        id: 0,
+        band: "The Rolling Stones",
+        origin: "Reino Unido",
+        description: "GENERAL.ONE-TEXT-DESCRIPTION",
+        image: "../../assets/image/rolling.jpg",
+        spotify: "https://open.spotify.com/playlist/2M7YeNwuRkDfTJYYwhPCkR",
+        imusic: "https://music.apple.com/es/artist/the-rolling-stones/1249595",
+        youtube: "https://www.youtube.com/embed/YzjZEci-EiU",
+        web: "https://rollingstones.com/",
+        members: "Mick Jagger, Keith Richards, Charlie Watts, Ronnie Wood",
+        exmembers: "Brian Jones, Ian Stewart, Bill Wyman, Mick Taylor, Dick Taylor, Tony Chapman, Mick Avory"
+    },
+
+    {
+        id: 1,
+        band: "Led Zeppelin",
+        origin: "Reino Unido",
+        description: "GENERAL.TWO-TEXT-DESCRIPTION",
+        image: "../../assets/image/led.jpg",
+        spotify: "https://open.spotify.com/playlist/2MTWsZURWTG0kPXVLPMxGH",
+        imusic: "https://music.apple.com/es/artist/led-zeppelin/994656",
+        youtube: "https://www.youtube.com/embed/sfR_HWMzgyc",
+        members: "Jimmy Page, Robert Plant, John Paul Jones, John Bonham",
+        exmembers: null,
+        web: "https://lz50.ledzeppelin.com/?ref=https://www.google.com/"
+    },
+
+    {
+        id: 2,
+        band: "Queen",
+        origin: "Reino Unido",
+        description: "GENERAL.THREE-TEXT-DESCRIPTION",
+        image: "../../assets/image/queen.png",
+        spotify: "https://open.spotify.com/playlist/595cY5S1FpMpiEIWsk6dG7",
+        imusic: "https://music.apple.com/es/artist/queen/3296287",
+        youtube: "https://www.youtube.com/embed/rY0WxgSXdEE",
+        members: "Brian May, Roger Taylor",
+        exmembers: "Freddie Mercury, John Deacon",
+        web: "https://www.queenonlinestore.com/ "
+    },
+
+    {
+        id: 3,
+        band: "Pink Floyd",
+        origin: "Reino Unido",
+        description: "GENERAL.FOUR-TEXT-DESCRIPTION",
+        image: "../../assets/image/pink.jpg",
+        spotify: "https://open.spotify.com/playlist/37i9dQZF1DXaQ34lqGBfrU ",
+        imusic: "https://music.apple.com/es/artist/pink-floyd/487143",
+        youtube: "https://www.youtube.com/embed/2YopyMU2SYA",
+        members: "David Gilmour, Nick Mason, Richard Wright",
+        exmembers: "Syd Barrett, Roger Waters, Bob Klose",
+        web: "https://www.pinkfloyd.com/home.php"
+    },
+
+    {
+        id: 4,
+        band: "Deep Purple",
+        origin: "Reino Unido",
+        description: "GENERAL.FIVE-TEXT-DESCRIPTION",
+        image: "../../assets/image/deep.jpg",
+        spotify: "https://open.spotify.com/playlist/37i9dQZF1DX5WtNRbfbuUo ",
+        imusic: "https://music.apple.com/es/artist/deep-purple/135532 ",
+        youtube: "https://www.youtube.com/embed/mhqeZM-X9-k",
+        members: "Ian Paice, Ian Gillan, Roger Glover, Steve Morse, Don Airey",
+        exmembers: "Jon Lord, Ritchie Blackmore, Rod Evans, Nick Simper, Glenn Hughes, David Coverdale, Tommy Bolin, Joe Lynn Turner, Joe Satriani",
+        web: "https://deeppurple.com/"
+    },
+
+    {
+        id: 5,
+        band: "AC/DC",
+        origin: "Australia",
+        description: "GENERAL.SIX-TEXT-DESCRIPTION",
+        image: "../../assets/image/ac.jpg",
+        spotify: "https://open.spotify.com/playlist/37i9dQZF1DXec50AjHrNTq ",
+        imusic: "https://music.apple.com/es/artist/ac-dc/5040714",
+        youtube: "https://www.youtube.com/embed/v2AC41dglnM",
+        members: "Angus Young, Stevie Young, Brian Johnson, Cliff Williams, Phil Rudd",
+        exmembers: "Bon Scott, Malcolm Young, Simon Wright,Mark Evans,Dave Evans",
+        web: " http://www.ac-dc.net/"
+    },
+
+    {
+        id: 6,
+        band: "The Ramones",
+        origin: "Estados Unidos",
+        description: "GENERAL.SEVEN-TEXT-DESCRIPTION",
+        image: "../../assets/image/ramones.png",
+        spotify: "https://open.spotify.com/artist/1co4F2pPNH8JjTutZkmgSm",
+        imusic: "https://music.apple.com/es/artist/ramones/60715",
+        youtube: "https://www.youtube.com/embed/F3J0iwwsq-w?start=21",
+        members: "Joey Ramone, Johnny Ramone,Marky Ramone, Elvis Ramone,C. J. Ramone,Dee Dee Ramone, Ramone, Richie Ramone",
+        exmembers: null,
+        web: "https://www.ramones.com/"
+    },
+
+    {
+        id: 7,
+        band: "Kiss",
+        origin: "Estados Unidos",
+        description: "GENERAL.EIGHT-TEXT-DESCRIPTION",
+        image: "https://www.ecured.cu/images/thumb/2/25/Kiss.jpg/260px-Kiss.jpg",
+        spotify: "https://open.spotify.com/artist/07XSN3sPlIlB2L2XNcTwJw",
+        imusic: "https://music.apple.com/es/artist/kiss/118108",
+        youtube: "https://www.youtube.com/embed/sDCw4fj_TsA",
+        members: "Paul Stanley, Gene Simmons, Eric Singer, Tommy Thayer",
+        exmembers: "Ace Frehley, Peter Criss, Eric Carr, Vinnie Vincent, Mark St. John, Bruce Kulick",
+        web: "https://www.kissonline.com/"
+    }
+
+]
